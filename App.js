e80233925c18f57b3bed9b5dff3b11f6383589f9
Brooke Wilson <brookewil@comcast.net>
2025-03-31 10:46:07 -0400

    created base username login

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Form } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  
+  const [username, setUsername] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Text>Username:</Text>
+        <TextInput 
+          id="username" 
+          value={username.value} 
+          onChangeText={(text) => setUsername(text)} 
+          placeholder="Enter Username" 
+        />
       <StatusBar style="auto" />
     </View>
   );
