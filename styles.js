17c02d991f85f8ba17d1f76fe6667394573eeed2
Brooke Wilson <brookewil@comcast.net>
2025-04-01 10:25:43 -0400

    added seperate style sheet

---
+++ styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    padding: 20
+  },
+
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    width: '100%' 
+  },
+
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    marginTop: 10
+    }
+}
+)
+
