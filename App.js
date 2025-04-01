b1a5e886145adf645d0ce592442f9056b749b1da
Brooke Wilson <brookewil@comcast.net>
2025-04-01 10:37:40 -0400

    added validateEmail method

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Form, Button } from 'react-native';
+import { Text, View, TextInput, Form, Button } from 'react-native';
 import React, { useState } from 'react';
 import {styles} from './styles.js';
 
 export default function App() {
   
-  const [username, setUsername] = useState('');
+  //const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  // add email validation method, check isValid, and then alert "welcome"
+  const validateEmail = (email) => {
+    const emailRegex = "^(?!.*[._%+-]{2})[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    return emailRegex.test(email);
+  }
 
   const handleLogin = () => {
-    if (username === 'username' && password === 'password') {
+    if (email.validateEmail == true && email === 'email' && password === 'password') {
       alert('Welcome');
     } else {
-      alert('Incorrect Username or Password');
+      alert('Invalid Email or Password');
     }
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Enter Username</Text>
+
+      <Text style={styles.title}>Enter Email</Text>
         <TextInput 
           style={styles.input}
-          value={username}
-          onChangeText={setUsername}
-          placeholder="Username"
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Email"
         />
 
       <Text style={styles.title}>Enter Password</Text>
