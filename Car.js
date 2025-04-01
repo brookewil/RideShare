af02d2a1a5d29b5efe7cd75cd4d7ae409820ff2c
clapierre22 <clapierre@ithaca.edu>
2025-04-01 09:59:41 -0400

    Added Driver.js, Car.js with constructors and low level logic

---
+++ Car.js
@@ -0,0 +1,14 @@
+class Car {
+    constructor(year, make, model, color, plate) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.plate = plate;
+    }
+
+    getDetails() {
+        return [this.year, this.make, this.model, this.color, this.plate];
+    }
+
+}
--- Driver.js
+++ Driver.js
@@ -2,5 +2,29 @@ class Driver {
     constructor(username) {
         this.username = username;
         this.approved = false;
+        this.cars = [];
+    }
+
+    signup() {
+        const date = new Date();
+        console.log(`Driver ${this.username} signed up at ${date}`);
+    }
+
+    approve() {
+        const date = new Date();
+        this.approved = true;
+        console.log(`Driver ${this.username} approved on ${date}`);
+    }
+
+    remove() {
+        const date = new Date();
+        this.approved = false;
+        console.log(`Driver ${this.username} removed on ${date}`)
+    }
+
+    addCar(car) {
+        const date = new Date();
+        this.cars.push(car);
+        console.log(`Car added to ${this.username}'s account ON ${date}`);
     }
 }
