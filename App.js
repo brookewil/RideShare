6717f7c3bf0128a19b9a2cfe5b40e49691a7760b
Brooke Wilson <brookewil@comcast.net>
2025-04-16 10:42:37 -0400

    created Driver screen

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import UserHomeScreen from './screens/UserHomeScreen';
+import AdminHomeScreen from './screens/AdminHomeScreen';
+import DriverHomeScreen from './screens/DriverHomeScreen';
 
 const Stack = createNativeStackNavigator();
 
---
+++ screens/DriverHomeScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+
+function DriverAdminHomeScreen() {
+  return (
+     <View style={{ flex: 1, alignItems:   
+           'center', justifyContent: 'center' }}>
+       <Text>Driver Home Screen</Text>
+     </View>
+  );
+}
+
+export default DriverHomeScreen;
