70450f59a6eed7de097cdde3cc1c8bb1fa3d55bb
Brooke Wilson <brookewil@comcast.net>
2025-04-16 10:32:32 -0400

    Renamed files and added navbar

--- App.js
+++ App.js
@@ -1,76 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, TextInput, Form, Button } from 'react-native';
-import React, { useState } from 'react';
-import {styles} from './styles.js';
-import { auth } from './firebaseConfig';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { Alert } from 'react-native'; 
-import SignUpScreen from './SignUpScreen.js'; // Import the SignUpScreen component
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
+const Stack = createNativeStackNavigator();
 
-
-
-import {initMap} from './Map.js';
-
-export default function App() {
-  
-  //const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [showSignup, setShowSignup] = useState(false); // to toggle view
-
-
-  // add email validation method, check isValid, and then alert "welcome"
-  const validateEmail = (email) => {
-    const emailRegex = /^(?!.*[._%+-]{2})[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
-  };
-
-  const handleLogin = async () => {
-    if (!validateEmail(email)) {
-      Alert.alert('Invalid Email Format');
-      return;
-    }
-
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      Alert.alert('Welcome');
-    } catch (error) {
-      Alert.alert('Login Failed', error.message);
-    }
-  };
- 
-  if (showSignup) {
+function App() {
     return (
-      <View style={styles.container}>
-        <SignUpScreen />
-        <Button title="Back to Login" onPress={() => setShowSignup(false)} />
-      </View>
-    );
-  }
-  return (
-    <View style={styles.container}>
-
-      <Text style={styles.title}>Enter Email</Text>
-        <TextInput 
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          placeholder="Email"
-        />
-
-      <Text style={styles.title}>Enter Password</Text>
-        <TextInput 
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-        />
-
-        <Button title="Submit" onPress={handleLogin} />
-        <Button title="Create Account" onPress={() => setShowSignup(true)} />
-
-      <StatusBar style="auto" />
-    </View>
-  );
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+     );
 }
+
+export default App;
---
+++ screens/LoginScreen.js
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, TextInput, Form, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import {styles} from '../styles.js';
+import { auth } from '../firebaseConfig.js';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Alert } from 'react-native'; 
+import SignUpScreen from '../SignUpScreen.js'; // Import the SignUpScreen component
+
+import {initMap} from '../Map.js';
+
+export default function LoginScreen() {
+  
+  //const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [showSignup, setShowSignup] = useState(false); // to toggle view
+  const navigation = useNavigation();
+
+  // add email validation method, check isValid, and then alert "welcome"
+  const validateEmail = (email) => {
+    const emailRegex = /^(?!.*[._%+-]{2})[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  };
+
+  const handleLogin = async () => {
+    if (!validateEmail(email)) {
+      Alert.alert('Invalid Email Format');
+      return;
+    }
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      Alert.alert('Welcome');
+      navigation.navigate('Home');
+
+    } catch (error) {
+      Alert.alert('Login Failed', error.message);
+    }
+  };
+ 
+  if (showSignup) {
+    return (
+      <View style={styles.container}>
+        <SignUpScreen />
+        <Button title="Back to Login" onPress={() => setShowSignup(false)} />
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Enter Email</Text>
+        <TextInput 
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Email"
+        />
+
+      <Text style={styles.title}>Enter Password</Text>
+        <TextInput 
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password"
+        />
+
+        <Button title="Submit" onPress={handleLogin}/>
+        <Button title="Create Account" onPress={() => setShowSignup(true)} />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
