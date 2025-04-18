3b4bc14f93bce57092cb99b6f516f1d85f743096
clapierre22 <clapierre@ithaca.edu>
2025-04-18 11:22:07 -0400

    Map now uses user location. However, it does NOT ask for permission before doing so

--- Map.js
+++ Map.js
@@ -1,28 +1,85 @@
-import React from 'react';
+//import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+// Map Imports
 //import {Map} from 'react-native-maps';
 import MapView, {Marker} from 'react-native-maps';
 import {StyleSheet, View} from 'react-native';
 
-let map;
+// Location Imports
+//import Geolocation from '@react-native-community/geolocation';
+import * as Location from 'expo-location';
+
+// Ithaca College Coords:
+// 42.422668, -76.494209
+
+async function UserLocation() {
+    // Get User Location
+    try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            console.error('Location Permission was denied');
+            return;
+        }
+
+        const location = await Location.getCurrentPositionAsync({});
+        console.log('User Location:', location.coords.latitude, location.coords.longitude);
+
+        return location.coords;
+    } catch (error) {
+        console.error('Could not retrieve user location:', error.message);
+    }
+}
 
 function MapRS() {
     // Initialize and create Map Object
+    const [location, setLocation] = React.useState(null);
+    useEffect(() => {
+        (async () => {
+            const userLocation = await UserLocation();
+            if (userLocation) {
+                setLocation(userLocation);
+            }
+        })();
+    }, []);
+
+    if (!location){
+        return (
+            <View style = {styles.container}>
+                <MapView
+                    style = {styles.map}
+                    initialRegion = {{
+                        latitude: 42.4226,
+                        longitude: -76.4942,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                </MapView>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
             <MapView
                 style={styles.map}
+                // Shows user location
                 initialRegion={{
                     // TODO: Take lat, long from User Location
-                    latitude: 42.4439,
-                    longitude: -76.5019,
+                    latitude: location.latitude,
+                    longitude: location.longitude,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
             >
                 <Marker
-                    coordinate={{latitude: 42.4439, longitude: -76.5019}}
-                    title="RideShare"
-                    description="This is Ithaca, NY"
+                    coordinate={{
+                        latitude: location.latitude, 
+                        longitude: location.longitude,
+                    }}
+                    title="Your Location"
+                    description="Disclaimer: User Position may not be Exact"
                 />
             </MapView>
         </View>
--- screens/UserHomeScreen.js
+++ screens/UserHomeScreen.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
-
+import MapRS from '../Map'; 
 
 function UserHomeScreen() {
   return (
      <View style={{ flex: 1, alignItems:   
            'center', justifyContent: 'center' }}>
-       <Text>User Home Screen</Text>
+        <Text>User Home Screen</Text>
+        <View style = {{ height: '80%', width: '90%', overflow: 'hidden' }}>
+          <Text>Map</Text>
+          <MapRS />
+        </View>
      </View>
   );
 }
