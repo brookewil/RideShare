2cc5b0cbc873b372327fcd861c72c95a5dfb6090
Brooke Wilson <brookewil@comcast.net>
2025-04-22 07:30:07 -0400

    added base content to page

--- screens/RequestScreen.js
+++ screens/RequestScreen.js
@@ -3,7 +3,12 @@ import { View, Text } from 'react-native';
 import {styles} from '../styles.js';
 
 function RequestScreen(){
-
+    return (
+         <View style={{ flex: 1, alignItems:   
+               'center', justifyContent: 'center' }}>
+           <Text>Request Screen</Text>
+         </View>
+      );
 }
 
 export default RequestScreen;
