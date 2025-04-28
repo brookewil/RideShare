2aa8aa88487078cb2c629f0f181b10554b05da37
Brooke Wilson <brookewil@comcast.net>
2025-04-28 10:54:00 -0400

    added navbar icons

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import LoginScreen from './screens/LoginScreen';
 import UserHomeScreen from './screens/UserHomeScreen';
 import AdminHomeScreen from './screens/AdminHomeScreen';
@@ -11,23 +12,55 @@ import PlanRideScreen from './screens/PlanRideScreen';
 import RideStatusScreen from './screens/RideStatusScreen';
 
 const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+   
+    return (
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="AdminHome" component={AdminHomeScreen} />
+        <Stack.Screen name="DriverHome" component={DriverHomeScreen} />
+        <Stack.Screen name="Ride Status" component={RideStatusScreen} />
+      </Stack.Navigator>
+    )
+}
+
 const Tab = createBottomTabNavigator();
 
-function App() {
+export function TabNavigator() {
+  return (
+        <Tab.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Tab.Screen name="UserHome" component={UserHomeScreen} 
+            options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+            ),
+        }} />
+            <Tab.Screen name="Request" component={RequestScreen} 
+            options={{
+            tabBarLabel: 'Request',
+            tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" color={color} size={size} />
+            ),
+        }} />
+            <Tab.Screen name="Plan a Ride" component={PlanRideScreen} 
+            options={{
+                tabBarLabel: 'Plan a Ride',
+                tabBarIcon: ({ color, size }) => (
+                <Ionicons name="alarm" color={color} size={size} />
+            ),
+        }} />
+        </Tab.Navigator>
+   )
+}
+
+export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="UserHome" component={UserHomeScreen} />
-                <Stack.Screen name="AdminHome" component={AdminHomeScreen} />
-                <Stack.Screen name="DriverHome" component={DriverHomeScreen} />
-                <Stack.Screen name="Request" component={RequestScreen} />
-                <Stack.Screen name="PlanRide" component={PlanRideScreen} />
-                <Stack.Screen name="RideStatus" component={RideStatusScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+           <TabNavigator />
+         </NavigationContainer>
         
      );
 }
-
-export default App;
