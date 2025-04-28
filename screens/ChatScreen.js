add52061bd5f4fd11bf99846823c0d8afb79f471
Brooke Wilson <brookewil@comcast.net>
2025-04-28 11:30:30 -0400

    created chat screen

---
+++ screens/ChatScreen.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import {styles} from '../styles.js';
+
+
+function ChatScreen() {
+  return (
+     <View style={{ flex: 1, alignItems:   
+           'center', justifyContent: 'center' }}>
+       <Text>Chat Screen</Text>
+     </View>
+  );
+}
+
+export default ChatScreen;
