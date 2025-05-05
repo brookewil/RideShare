b68f4c907e1c1b5e9a93d5cbe2314876060a6d7b
Brooke Wilson <brookewil@comcast.net>
2025-05-05 19:35:29 -0400

    fixed styling

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { View, Text, Button, FlatList, Image} from 'react-native';
+import { View, Text, Button, FlatList, Image, TouchableOpacity} from 'react-native';
 import {styles} from '../styles.js';
-import styled from 'styled-components/native';
 
 const Messages = [
     {
@@ -22,80 +21,27 @@ const Messages = [
 
 const MessageScreen = ({navigation}) => {
     return (
-        <Container>
+        <View style={styles.Container}>
             <FlatList
                 data={Messages}
                 keyExtractor={item=>item.id}
                 renderItem={({item}) => (
-                    <Card onPress={() => navigation.navigate('Chat')}>
-                        <UserInfo>
-                            <UserImgWrapper>
-                                <UserImg source={item.userImg}/>
-                            </UserImgWrapper>
-                            <TextSection>
-                                <UserInfoText>
-                                    <UserName>{item.userName}</UserName>
-                                </UserInfoText>
-                            </TextSection>
-                        </UserInfo>
-                    </Card>
+                    <TouchableOpacity style={styles.Card} onPress={() => navigation.navigate('Chat')}>
+                        <View style={styles.UserInfo}>
+                            <View style={styles.UserImgWrapper}>
+                                <Image style={styles.UserImg} source={item.userImg}/>
+                            </View>
+                            <View style = {styles.TextSection}>
+                                <View style={styles.UserInfoText}>
+                                    <Text style={styles.UserName}>{item.userName}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
                 )}
             />
-        </Container>
+        </View>
     );
 };
-
-// Message Styling
-
-const Container = styled.View`
-    flex: 1;
-    padding-left: 20px;
-    padding-right: 20px;
-    align-items: center;
-    background-color: #ffffff
-`;
-
-const Card = styled.TouchableOpacity`
-    width: 100%
-`;
-
-const UserInfo = styled.View`
-    flex-direction: row;
-    justify-content: space-between;
-`;
-
-const UserImgWrapper = styled.View`
-    padding-top: 15px;
-    padding-bottom: 15px;
-`;
-
-const UserImg = styled.Image`
-    width: 50px;
-    height 50px;
-    border-radius: 25px;
-`;
-
-const TextSection = styled.View`
-    flex-direction: column;
-    justify-content: center;
-    padding: 15px;
-    padding-left: 0;
-    margin-left: 10px;
-    width: 300px;
-    border-bottom-width: 1px;
-    border-bottom-color: #cccccc;
-`;
-
-const UserInfoText = styled.View`
-    flex-direction: row;
-    justify-content: space-between;
-    margin-bottom: 5px;
-`;
-
-const UserName = styled.Text`
-    font-size: 14px;
-    font-weight: bold;
-`;
-
 
 export default MessageScreen;
--- styles.js
+++ styles.js
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-import styled from 'styled-components';
 
 export const styles = StyleSheet.create({
   loginContainer: {
@@ -115,6 +114,59 @@ export const styles = StyleSheet.create({
   
   navigation: {
     display: 'none',
+  },
+
+  // Message Styling
+
+  Container: {
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+
+  },
+  
+  Card: {
+    width: '100%',
+  },
+  
+  UserInfo: {
+    flexDirection: 'row',
+    justifyContent: 'spaceBetween',
+  },
+  
+  UserImgWrapper: {
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+
+  UserImg: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  
+  TextSection: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 15,
+    paddingLeft: 0,
+    marginLeft: 10,
+    width: 300,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  
+  UserInfoText:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  
+  UserName: {
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 
 }
