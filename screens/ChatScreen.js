b29544cc47852a12edeb72aeee6dd2955c61bc7a
Brooke Wilson <brookewil@comcast.net>
2025-05-05 20:09:48 -0400

    added giftedChat tempate and package

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,15 +1,38 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import {styles} from '../styles.js';
+import {GiftedChat} from 'react-native-gifted-chat'
 
 
-function ChatScreen() {
+export function ChatScreen() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id:1,
+        text: 'Hello Developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any,'
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
-     <View style={{ flex: 1, alignItems:   
-           'center', justifyContent: 'center' }}>
-       <Text>Chat Screen</Text>
-     </View>
-  );
+    <GiftedChat
+    messages={messages}
+    onSend={messages => onSend(messages)}
+    user={{
+      _id: 1,
+    }}
+    />
+  )
 }
-
-export default ChatScreen;
